1c52f5b61b8bb684bea0ed97e31742e5671f1ec8
billaagustriani <156525387+billaagustriani@users.noreply.github.com>
2024-03-25 22:21:08 +0700

    Update brush_gerak.ino

--- nice_porppeler.ino
+++ nice_porppeler.ino
@@ -1,22 +1,12 @@
-// Definisi pin yang digunakan
-#include <ESP32Servo.h>
-
-Servo motor3, motor4, motor5, motor6;
-
-void setup() {
-  // Inisialisasi Serial Monitor dengan kecepatan 9600 bps
-  Serial.begin(115200);
-  // Atur pin sebagai output
+void setup_motor(){
+  motor1.attach(19);
+  motor2.attach(0);
   motor3.attach(4);
   motor4.attach(18);
   motor5.attach(5);
   motor6.attach(16);
 }
-
-void loop() {
-  // Tampilkan instruksi pada Serial Monitor
-  //Serial.println("Ketik 'R' untuk menggerakkan motor ke kanan atau 'L' untuk ke kiri:");
-  
+void gerak(){
   if(Serial.available() > 0){
    char command = Serial.read();
   
@@ -40,7 +30,9 @@ void loop() {
     }
 
     //Matikan Brussles 
-     else if (command == 'B' || command == 'b'){
+    else if (command == 'B' || command == 'b'){
+    motor1.writeMicroseconds(1500); 
+    motor2.writeMicroseconds(1500); 
     motor3.writeMicroseconds(1500);
     motor4.writeMicroseconds(1500);
     motor5.writeMicroseconds(1500);
@@ -66,5 +58,18 @@ void loop() {
     motor6.writeMicroseconds(1700); // Kecepatan penuh ke depan/cw
     delay(1000);
     }
+
+    else if (command == 'N' || command == 'n'){
+      motor1.writeMicroseconds(1300); //CW
+      motor2.writeMicroseconds(1700); //CCW
+      delay(1000);
+    }
+
+    else if (command == 'T' || command == 't' ){
+      motor1.writeMicroseconds(1700); //CW
+      motor2.writeMicroseconds(1300); //CCW
+      delay(1000);
+    }
+
   }
 }
