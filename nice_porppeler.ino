c35b0ad49099d5c56fa35ab79abbb8f6384d07a7
billaagustriani <156525387+billaagustriani@users.noreply.github.com>
2024-03-25 20:26:26 +0700

    Update nice_porppeler.ino

--- nice_porppeler.ino
+++ nice_porppeler.ino
@@ -1,16 +1,16 @@
 // Definisi pin yang digunakan
-#include <Servo.h>
+#include <ESP32Servo.h>
 
-char command;
-
-Servo motor1;
+Servo motor3, motor4, motor5, motor6;
 
 void setup() {
-  // Atur pin sebagai output
-  motor1.attach(10);
-
   // Inisialisasi Serial Monitor dengan kecepatan 9600 bps
   Serial.begin(115200);
+  // Atur pin sebagai output
+  motor3.attach(4);
+  motor4.attach(18);
+  motor5.attach(5);
+  motor6.attach(16);
 }
 
 void loop() {
@@ -18,24 +18,53 @@ void loop() {
   //Serial.println("Ketik 'R' untuk menggerakkan motor ke kanan atau 'L' untuk ke kiri:");
   
   if(Serial.available() > 0){
-    command = Serial.read();
-  }
+   char command = Serial.read();
   
-  // Brussless kanan
-  else if (command == 'R' || command == 'r'){
-    motor1.writeMicroseconds(2000);
-    delay(2000);
-  }
-
-  // brushless kiri
-  else if (command == 'L' || command == 'l'){
-    motor1.writeMicroseconds(1000);
-    delay(2000);
-  }
-
-  //Matikan Brussles 
-  else if (command == 'B' || command == 'b'){
-    motor1.writeMicroseconds(1500);
+  
+    // Brussless kanan
+    if (command == 'R' || command == 'r'){
+    motor3.writeMicroseconds(1700);//CCW
+    motor4.writeMicroseconds(1700);//CW
+    motor5.writeMicroseconds(1700);//CCW
+    motor6.writeMicroseconds(1700);//CW
     delay(1000);
+    }
+
+    // brushless kiri
+    else if (command == 'L' || command == 'l'){
+    motor3.writeMicroseconds(1300);
+    motor4.writeMicroseconds(1300);
+    motor5.writeMicroseconds(1300);
+    motor6.writeMicroseconds(1300);
+    delay(1000);
+    }
+
+    //Matikan Brussles 
+     else if (command == 'B' || command == 'b'){
+    motor3.writeMicroseconds(1500);
+    motor4.writeMicroseconds(1500);
+    motor5.writeMicroseconds(1500);
+    motor6.writeMicroseconds(1500);
+    delay(1000);
+    }
+  
+
+    else if (command == 'M' || command == 'm'){
+    ///gerak maju
+    motor3.writeMicroseconds(1700); // Kecepatan penuh ke depan//CCW
+    motor4.writeMicroseconds(1300); // Kecepatan penuh ke depan//CW
+    motor5.writeMicroseconds(1700); // Kecepatan penuh ke belakang//cCW
+    motor6.writeMicroseconds(1300); // Kecepatan penuh ke belakang//CW
+    delay(1000);
+    }
+
+    else if (command == 'U' || command == 'u'){
+    ///gerak mundur
+    motor3.writeMicroseconds(1300); // Kecepatan penuh ke belakang/ccw
+    motor4.writeMicroseconds(1700); // Kecepatan penuh ke belakang/cw
+    motor5.writeMicroseconds(1300); // Kecepatan penuh ke depan/ccw
+    motor6.writeMicroseconds(1700); // Kecepatan penuh ke depan/cw
+    delay(1000);
+    }
   }
 }
